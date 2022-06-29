7464ca7157fcfb7460aa814ac92768bbc7b0c1e2
Amanda Briones <amandabriones111@gmail.com>
2022-06-28 18:50:59 -0700

    commit tree 00e6c4e3f115a76bf7cb5c8a6de6c18ca4f4cf99

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+const hq = 42
+const feet = 264
+
+function distanceFromHqInBlocks(bpickup) {
+    if (bpickup > hq) 
+        return bpickup - hq
+    else (bpickup < hq)
+        return hq - bpickup
+}
+
+function distanceFromHqInFeet(fpickup) {
+    if (fpickup > hq) 
+        return (fpickup - hq)*264;
+    else (fpickup < hq)
+        return (hq - fpickup)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination)
+        return (start - destination)*264;
+    else (start < destination)
+        return (destination - start)*264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let blockDistance = Math.abs(start - destination);
+    let distanceTraveled = blockDistance * 264;
+    if (distanceTraveled <= 400) {
+        console.log('give customers a free sample');
+        return 0
+    }
+    else if (distanceTraveled >= 2500) {
+        return 'cannot travel that far';
+    }
+    else if (400 < distanceTraveled && distanceTraveled < 2000) {
+        let rideCost = (0.02 * (distanceTraveled - 400));
+            return rideCost;
+    }
+    else if (2000 <= distanceTraveled < 2500) {
+        return 25;
+    }
+    }
